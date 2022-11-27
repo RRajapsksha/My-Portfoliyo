ba237a5a1c3f1692b9811d5938665e89be53464b
RRajapaksha <renukarajapaksha@gmail.com>
2022-11-27 21:11:10 +0530

    Content Update

--- assignments/js/game/assets/js/script.js
+++ assignments/js/game/assets/js/script.js
@@ -42,7 +42,7 @@ function runAnimation() {
 
 function runAnimationStart() {
 
-    runAnimationNumber = setInterval(runAnimation,200);
+    runAnimationNumber = setInterval(runAnimation,100);
 
     clearInterval(idleAnimationNumber);
 
@@ -51,27 +51,28 @@ function runAnimationStart() {
 
 jumpImageNumber = 1;
 jumpAnimationNumber = 0;
-boyMarginTop = 307;
+boyMarginTop = 240;
 
 function jumpAnimation() {
 
     jumpImageNumber = jumpImageNumber +1;
 
     if (jumpImageNumber <= 6){
-        boyMarginTop = boyMarginTop -20;
+        boyMarginTop = boyMarginTop -30;
         boy.style.marginTop = boyMarginTop + "px";
     }
 
     if (jumpImageNumber >= 7){
-        boyMarginTop = boyMarginTop +20;
+        boyMarginTop = boyMarginTop +40;
         boy.style.marginTop = boyMarginTop + "px";
     }
 
     if (jumpImageNumber == 13){
         jumpImageNumber = 1;
         clearInterval(jumpAnimationNumber);
-        jumImagNumber = 0;
-        jumpAnimationStart();
+        jumpAnimationNumber = 0;
+        runImageNumber = 0;
+        runAnimationStart();
     }
     boy.src = "assets/images/Jump("+jumpImageNumber+").png";
 }
@@ -87,63 +88,125 @@ function keyCheck(event) {
     // alert(event.which);
     // enter =13
     // space = 32
+
     var keyCode = event.which;
 
-    if (keyCode == 13){
+    if (keyCode == 13) {
 
-        if (runAnimationNumber == 0){
+        if (runAnimationNumber == 0) {
             runAnimationStart();
         }
 
-        if (moveBackgroundAnimationId == 0){
-            moveBackgroundAnimationId = setInterval(moveBackground,100);
+        if (moveBackgroundAnimationId == 0) {
+            moveBackgroundAnimationId = setInterval(moveBackground, 200);
+        }
+        if (boxAnimationId == 0) {
+            boxAnimationId = setInterval(boxAnimation, 200);
         }
     }
 
-    if (keyCode == 32){
-        if (jumpAnimationNumber == 0){
+    if (keyCode == 32) {
+
+        if (jumpAnimationNumber == 0) {
             jumpAnimationStart();
         }
 
-        if (moveBackgroundAnimationId == 0){
-            moveBackgroundAnimationId = setInterval(moveBackground,100);
-        }
+    if (moveBackgroundAnimationId == 0) {
+        moveBackgroundAnimationId = setInterval(moveBackground, 200);
     }
+    if (boxAnimationId == 0) {
+        boxAnimationId = setInterval(boxAnimation, 200);
+    }
+
+}
 
 }
 
 var backgroundImagePositionX = 0;
 var moveBackgroundAnimationId = 0;
 
+var score=0;
+
 function moveBackground() {
 
     backgroundImagePositionX = backgroundImagePositionX - 20;
     document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX+"px";
 
+    score =score+1;
+    document.getElementById("score").innerHTML = score;
 }
-boxMarginLeft = 1040;
+boxMarginLeft = 1400;
 
 function createBoxes() {
-    for (var i =0;i<=10;i++){
+
+    for (var i=0;i<=10;i++){
         var box=document.createElement("div");
         box.className = "box";
         document.getElementById("background").appendChild(box);
         box.style.marginLeft = boxMarginLeft + "px";
-
+        box.id ="box"+i;
+        
         // boxMarginLeft =boxMarginLeft +1000;
 
         if (i <5){
-            boxMarginLeft = boxMarginLeft + 500;
+            boxMarginLeft = boxMarginLeft + 1500;
         }
 
         if (i>=5){
-            boxMarginLeft = boxMarginLeft + 250;
+            boxMarginLeft = boxMarginLeft + 500;
         }
         }
+}
 
+var boxAnimationId = 0;
 
+function boxAnimation() {
+
+    for (var i=0;i<10;i++){
+
+        var box = document.getElementById("box"+i);
+        var currentMarginLeft = getComputedStyle(box).marginLeft;
+        var newMarginLeft = parseInt(currentMarginLeft)-25;
+        box.style.marginLeft = newMarginLeft + "px";
+
+        if (newMarginLeft >= -110 & newMarginLeft <=100){
+            if(boyMarginTop>300){
+                clearInterval(boxAnimationId);
+
+                clearInterval(runAnimationNumber);
+                runAnimationNumber=-1;
+
+                clearInterval(jumpAnimationNumber);
+                jumpAnimationNumber=-1;
+
+                clearInterval(moveBackgroundAnimationId);
+                moveBackgroundAnimationId=-1;
+
+                deadAnimationNumber =setInterval(boyDeadAnimation,100);
+
+            }
+        }
+    }
 
 }
+
+// dead
+
+deadImageNumber=1
+deadAnimationNumber=0;
+
+function  boyDeadAnimation(){
+    deadImageNumber=deadImageNumber+1;
+    if (deadImageNumber ==11){
+        deadImageNumber = 10;
+    }
+
+    boy.src ="assets/images/Dead("+deadImageNumber+").png"
+}
+
+
+
+
 
 
 
