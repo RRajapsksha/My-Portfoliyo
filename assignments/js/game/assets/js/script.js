229cc885029b836b958afaa45ed6ec66ef8920d5
RRajapaksha <renukarajapaksha@gmail.com>
2022-11-27 10:51:10 +0530

    COntent Update

--- assignments/js/game/assets/js/script.js
+++ assignments/js/game/assets/js/script.js
@@ -1,6 +1,6 @@
 var boy = document.getElementById("boy");
 
-var idleImageNumber = 0;
+var idleImageNumber = 1;
 var idleAnimationNumber = 0;
 
 function idleAnimation() {
@@ -22,7 +22,7 @@ function idleAnimationStart() {
     idleAnimationNumber=setInterval(idleAnimation,200);
 }
 
-runImageNumber=0;
+runImageNumber=1;
 runAnimationNumber=0;
 
 function runAnimation() {
@@ -44,10 +44,33 @@ function runAnimationStart() {
 
 }
 
+jumpImageNumber = 1;
+jumpAnimationNumber = 0;
+
+function jumpAnimation() {
+
+    jumpImageNumber = jumpImageNumber +1;
+
+    if (jumpImageNumber == 13){
+        jumpImageNumber = 1;
+        clearInterval(jumpAnimationNumber);
+        jumImagNumber = 0;
+        jumpAnimationStart();
+    }
+    boy.src = "assets/images/Jump("+jumpImageNumber+").png";
+}
+function jumpAnimationStart() {
+    clearInterval(idleAnimationNumber);
+    runImageNumber = 0;
+    clearInterval(runAnimationNumber);
+    jumpAnimationNumber = setInterval(jumpAnimation,100);
+}
+
 function keyCheck(event) {
 
     // alert(event.which);
     // enter =13
+    // space = 32
     var keyCode = event.which;
 
     if (keyCode === 13){
@@ -55,5 +78,31 @@ function keyCheck(event) {
         if (runAnimationNumber === 0){
             runAnimationStart();
         }
+
+        if (moveBackgroundAnimationId === 0){
+            moveBackgroundAnimationId = setInterval(moveBackground,100);
+        }
     }
+
+    if (keyCode == 32){
+        if (jumpAnimationNumber == 0){
+            jumpAnimationStart();
+        }
+
+        if (moveBackgroundAnimationId === 0){
+            moveBackgroundAnimationId = setInterval(moveBackground,100);
+        }
+    }
+
 }
+
+var backgroundImagePositionX = 0;
+var moveBackgroundAnimationId = 0;
+
+function moveBackground() {
+
+    backgroundImagePositionX = backgroundImagePositionX - 20;
+    document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX+"px";
+
+}
+
